ecf64048970109c7240495ab3dc5471b5ad5d1eb
KhallilB <khallil.bailey@presidioidentity.com>
2021-12-14 20:16:52 -0500

    update user schema

--- models/Users.js
+++ models/Users.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name: {
+    firstName: {
         type: String,
+        required: true
+    },
+    lastName: {
+        type: String, 
         required: true
     },
     birthday: {
